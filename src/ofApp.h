903079c44641a87e692ff5e0d051749966b9f709
Kyle Geske <stungeye@gmail.com>
2021-11-14 11:01:13 -0600

    Removed unused ofApp methods.

--- src/ofApp.h
+++ src/ofApp.h
@@ -13,17 +13,9 @@ public:
 
 	void keyPressed(int key) override;
 	void keyReleased(int key) override;
-	void mouseMoved(int x, int y) override;
-	void mouseDragged(int x, int y, int button) override;
-	void mousePressed(int x, int y, int button) override;
-	void mouseReleased(int x, int y, int button) override;
-	void mouseEntered(int x, int y) override;
-	void mouseExited(int x, int y) override;
 	void windowResized(int w, int h) override;
-	void dragEvent(ofDragInfo dragInfo) override;
-	void gotMessage(ofMessage msg) override;
 
 private:
-	const static int canvasWidth{ 800 }, canvasHeight{ 500 };
-	PongGame game{ {20, 100}, {20, 20}, {canvasWidth, canvasHeight}, 400 };
+	const static int canvasWidth{800}, canvasHeight{500};
+	PongGame game{{20, 100}, {20, 20}, {canvasWidth, canvasHeight}, 400};
 };
