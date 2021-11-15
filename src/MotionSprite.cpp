0aaa41fd3b9def4898b8d20e04351fd1836f573e
Kyle Geske <stungeye@gmail.com>
2021-11-15 10:39:57 -0600

    Some naming rework around bounce methods.

--- src/MotionSprite.cpp
+++ src/MotionSprite.cpp
@@ -3,49 +3,49 @@
 #include "MotionSprite.h"
 
 void MotionSprite::cruiseAt(const glm::vec2& newVelocity) {
-    velocity = newVelocity;
+	velocity = newVelocity;
 }
 
 void MotionSprite::accelerate(const glm::vec2& impulse) {
-    velocity += impulse;
+	velocity += impulse;
 }
 
 void MotionSprite::move(const float deltaTime) {
-    position += velocity * deltaTime;
+	position += velocity * deltaTime;
 }
 
-void MotionSprite::bounceHorizontalEdges(float ceilingY, float floorY) {
-    // Adjust ceiling and floor for ball size.
-    ceilingY += height / 2.0f;
-    floorY -= height / 2.0f;
+void MotionSprite::bounceHorizontalWithEdges(float ceilingY, float floorY) {
+	// Adjust ceiling and floor for ball size.
+	ceilingY += height / 2.0f;
+	floorY -= height / 2.0f;
 
-    // Check and execute bounce.
-    if (position.y <= ceilingY || position.y >= floorY) {
-        velocity.y *= -1;
-        position.y = ofClamp(position.y, ceilingY, floorY);
-    }
+	// Check and execute bounce.
+	if (position.y <= ceilingY || position.y >= floorY) {
+		velocity.y *= -1;
+		position.y = ofClamp(position.y, ceilingY, floorY);
+	}
 }
 
-void MotionSprite::bounce(const Sprite& other) {
-    const auto dy{position.y - other.position.y};
-    const auto hitDistanceY{0.5f * (height + other.height)};
+void MotionSprite::bounceVerticalWith(const Sprite& other) {
+	const auto dy{position.y - other.position.y};
+	const auto hitDistanceY{0.5f * (height + other.height)};
 
-    // Is the ball inline with the paddle vertically?
-    if (std::abs(dy) < hitDistanceY) {
-        const auto hitDistanceX{0.5f * (width + other.width)};
-        const auto dx{other.position.x - position.x};
+	// Is the ball inline with the other sprite vertically?
+	if (std::abs(dy) < hitDistanceY) {
+		const auto hitDistanceX{0.5f * (width + other.width)};
+		const auto dx{other.position.x - position.x};
 
-        // Has the ball cross over a paddle edge?
-        if (std::abs(dx) < hitDistanceX) {
-            // Reverse the X speed direction.
-            velocity.x *= -1;
+		// Has the ball cross over the other sprite's vertical edge?
+		if (std::abs(dx) < hitDistanceX) {
+			// Reverse the X speed direction.
+			velocity.x *= -1;
 
-            // Increase / Decrease Y speed depending on where we hit on paddle.
-            velocity.y += position.y - other.position.y;
+			// Increase / Decrease Y speed depending on where we hit the other sprite.
+			accelerate({0, position.y - other.position.y});
 
-            // Push ball away from paddle by the hit width.
-            const auto direction = dx / std::abs(dx); // Convert to either +1 or -1
-            position.x = other.position.x - (hitDistanceX * direction);
-        }
-    }
+			// Push ball away from colliding sprite by the hit width.
+			const auto direction = dx / std::abs(dx); // Convert to either +1 or -1
+			position.x = other.position.x - (hitDistanceX * direction);
+		}
+	}
 }
--- src/MotionSprite.h
+++ src/MotionSprite.h
@@ -3,15 +3,16 @@
 
 class MotionSprite : public Sprite {
 private:
-    glm::vec2 velocity;
+	glm::vec2 velocity;
 public:
-    MotionSprite(const float xPosition, const float yPosition, const float width, const float height, const float xSpeed, const float ySpeed)
-        : Sprite{xPosition, yPosition, width, height}, velocity{xSpeed, ySpeed} {
-    }
+	MotionSprite(const float xPosition, const float yPosition, const float width, const float height,
+	             const float xSpeed, const float ySpeed)
+		: Sprite{xPosition, yPosition, width, height}, velocity{xSpeed, ySpeed} {
+	}
 
-    void cruiseAt(const glm::vec2& newVelocity);
-    void accelerate(const glm::vec2& impulse);
-    void move(const float deltaTime);
-    void bounceHorizontalEdges(float ceilingY, float floorY);
-    void bounce(const Sprite& other);
+	void cruiseAt(const glm::vec2& newVelocity);
+	void accelerate(const glm::vec2& impulse);
+	void move(float deltaTime);
+	void bounceHorizontalWithEdges(float ceilingY, float floorY);
+	void bounceVerticalWith(const Sprite& other);
 };
